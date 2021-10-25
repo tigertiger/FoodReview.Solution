3155347a3c0b463ae8b671f487a48e1fd101792b
Melissa <melissa.scmi@gmail.com>
2021-10-25 11:04:29 -0700

    add validations
    Co-authored by: Ben Dunham <bendunhampdx@gmail.com>

--- Controllers/FoodsController.cs
+++ Controllers/FoodsController.cs
@@ -32,7 +32,68 @@ namespace FoodReview.Controllers
             _db.Foods.Add(food);
             await _db.SaveChangesAsync();
 
-            return CreatedAtAction(nameof(Get), new { id = food.FoodId }, food);
+            return CreatedAtAction(nameof(GetFood), new { id = food.FoodId }, food);
+        }
+        
+        // GET api/foods/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Food>> GetFood(int id)
+        {
+            var food = await _db.Foods.FindAsync(id);
+
+            if (food == null)
+            {
+                return NotFound();
+            }
+
+            return food;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, Food food)
+        {
+          if (id != food.FoodId)
+          {
+            return BadRequest();
+          }
+
+          _db.Entry(food).state = EntityState.Modified;
+
+          try
+          {
+            await _db.SaveChangesAsync();
+          }
+          catch (DbUpdateConcurrencyException)
+          {
+            if (!FoodExists(id))
+            {
+              return NotFound();
+            }
+            else
+            {
+              throw;
+            }
+          }
+          return NoContent();
+        }
+        // DELETE: api/Foods/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteFood(int id)
+        {
+          var food = await _db.Foods.FindAsync(id);
+          if (food == null)
+          {
+            return NotFound();
+          }
+          _db.Foods.Remove(food);
+          await _db.SaveChangesAsync();
+
+          return NoContent();
+          }
+
+        }
+        private bool FoodExists(int id)
+        {
+          return _db.Foods.Any(e => e.FoodId == id);
         }
     }
-}
--- Models/Food.cs
+++ Models/Food.cs
@@ -3,9 +3,14 @@ namespace FoodReview.Models
   public class Food
   {
     public int FoodId {get; set;}
+    [Required]
     public string Name {get; set;}
+    [Required]
     public string FoodGroup {get; set;}
+    [Required]
+    [Range(1,5, ErrorMessage = "This is a 5-Star system. Please rate between 1 and 5.")]
     public int Rating {get; set;}
+    [StringLength(10000)]
     public string Review {get; set;}
     public string ImageName {get; set;}
     public string ImageLocation {get; set;}
